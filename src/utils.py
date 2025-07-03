86de48e174df1b36b37e99e54711a1094ffeaeca
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-07-02 23:49:55 -0400

    feat: add device detection and image utils

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,30 @@
+from pathlib import Path
+from PIL import Image
 import torch
+
 
 def get_device():
     if torch.cuda.is_available():
-        device = torch.device('cuda')
-        print(f"Using GPU: {torch.cuda.get_device_name()}")
-        return device
-    else:
-        device = torch.device('cpu')
-        print("Using CPU")
-        return device
-        
+        return torch.device('cuda')
+    return torch.device('cpu')
+
+def convert_to_rgb(image_path: str):
+    image = Image.open(image_path)
+    
+    if image.mode == "RGBA":
+        image = image.convert('RGB')
+    return image
+
+def save_image(image: Image.Image, image_path: str):
+    """
+    Save PIL image to output directory with original filename from path
+    """
+    
+    output_dir = Path("output/")
+    output_dir.mkdir(exist_ok=True)
+    
+    filename = Path(image_path).name
+    final_path = output_dir / filename
+    image.save(final_path)
+    
+    
